eb69391da6dd54fe464847a96ddd559ae5cd1eba
NothingToSay0031 <jhwzju@gmail.com>
2025-09-02 02:44:52 -0700

    Quartz sync: Sep 2, 2025, 2:44 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -41,9 +41,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.Graph(),
   ],
 }
 
