9320edf7022bfb0a50cbeeedccaadeb2a3f41b3a
NothingToSay0031 <jhwzju@gmail.com>
2025-09-02 02:14:36 -0700

    Quartz sync: Sep 2, 2025, 2:14 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Blog",
-    pageTitleSuffix: "nts",
+    pageTitle: "电话微波炉",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
