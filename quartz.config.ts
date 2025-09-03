eebf11a90e137e99bbb545dce0bf83bcf7671d46
NothingToSay0031 <jhwzju@gmail.com>
2025-09-02 19:41:09 -0700

    Quartz sync: Sep 2, 2025, 7:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Notes",
+    pageTitle: "Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
